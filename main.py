18856b09d41880d3d2729480fccb654c47a59d5b
Arnanasas <arnas.bulk@gmail.com>
2024-05-24 15:22:19 +0300

    delete images after downloading

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 from selenium_renderer import SeleniumRenderer
 from database import PostgreSQLDatabase
+from tlp_calculator import TLP_calculator
 import configparser
 # Example usage
 if __name__ == "__main__":
 
     urls = [
         "https://facebook.com/login"
-        # # ("https://facebook.com/login", False),
-        # ("https://it112.lt/", False)
     ]
 
     # Initialize the ConfigParser
     config = configparser.ConfigParser()
-
-    # Read the .ini file
     config.read('config.ini')
-
     # Retrieve the DSN string directly
     dsn = config.get('database', 'dsn')
 
-    # Initialize the database
+    # Render and save url's
     db = PostgreSQLDatabase(dsn)
     with db:
-        db.create_tables()
+        # db.create_tables()
         renderer = SeleniumRenderer()
-        renderer.process_urls(urls)
+        renderer.render_and_save_url(urls)
+        calculator = TLP_calculator()
+        calculator.process_urls(urls)
